75fa50db6f058f10ce3ab36a7375e34ecc2bf2be
abiona01 <quadratwemimo@gmail.com>
2021-04-01 15:54:48 +0100

    Level 2 Exercise

--- src/App.js
+++ src/App.js
@@ -1,99 +1,23 @@
 import { Component } from "react";
-
-
-
-export default class  App extends Component {
-  componentDidMount(){
-    let winter = [11, 0, 1];
-    let spring = [2, 3, 4];
-    let summer = [5, 6, 7];
-    let autumn = [8, 9, 10];
-    const currentMonth = new Date().getMonth()
-    console.log(currentMonth)
-    if(winter.includes(currentMonth)){
-      //for morning
-      document.body.style.background = 'white'
-    }else if(spring.includes(currentMonth)){
-      //for afternoon
-      document.body.style.background = 'yellow'
-    }else if(summer.includes(currentMonth)){
-      //for night
-      document.body.style.background = 'black'
-    }else if(autumn.includes(currentMonth)){
-      document.body.style.background = 'red'
-    }
-  }
- render(){
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
- }
-}
-
-
-/**export default class  App extends Component {
-  componentDidMount(){
-    const currentTime = new Date().getHours()
-    console.log(currentTime)
-    if(currentTime >= 0 && currentTime < 12){
-      //for morning
-      document.body.style.background = 'white'
-    }else if(currentTime >= 12 && currentTime < 19){
-      //for afternoon
-      document.body.style.background = 'yellow'
-    }else{
-      //for night
-      document.body.style.background = 'black'
-    }
-  }
- render(){
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
- }
- 
-}*/
-/*
-import axios from "axios";
-import { Component } from "react";
-import "./styles.css";
+import "./style.css";
 
 export default class App extends Component {
   state = {
-    isLoading: true
+    isLoading: "Loading..."
   };
-  componentDidMount() {
-    setTimeout(() => {
-      axios.get("https://jsonplaceholder.typicode.com/todos/").then((res) => {
-        this.setState({
-          data: res.data,
-          isLoading: false
-        });
-      });
-    }, 3000);
-  }
+  onMouseEnter = () => {
+    let x = Math.random() * 500;
+    let y = Math.random() * 500;
+    document.body.style.transform = `translate(${x}px, ${y}px)`;
+  };
+
   render() {
-    const { isLoading } = this.state;
     return (
       <div className="App">
-        {isLoading ? (
-          <div>
-            <h1>Loading...</h1>
-          </div>
-        ) : (
-          <div>
-            <h1>Hello CodeSandbox</h1>
-            <h2>Start editing to see some magic happen!</h2>
-          </div>
-        )}
+        <div onMouseEnter={this.onMouseEnter} className="container">
+          <h1>30 Days Of React</h1>
+        </div>
       </div>
     );
   }
 }
-*/
