e6e117617c49cccf2ef07e81f4ff093ae921cbd3
abiona01 <quadratwemimo@gmail.com>
2021-04-20 14:26:12 +0100

    example

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from 'react'
+import axios from 'axios'
 
-// Home component
-const Home = (props) => <h1>Welcome Home</h1>
-// About component
-const About = (props) => <h1>About Us</h1>
-// Contact component
-const Contact = (props) => <h1>Contact us</h1>
-// Challenge component
-const Challenges = (props) => (
-  <div>
-    <h1>30 Days Of React Challenge</h1>
-  </div>
-)
-
-export default class App extends Component {
+ export default class Cat extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        data: []
+    }
+  }
+  componentDidMount() {
+    this.fetchCatData()
+  }
+  fetchCatData = async () => {
+    const url = 'https://api.thecatapi.com/v1/breeds'
+    try {
+      const response = await axios.get(url)
+      const data = await response.data
+      console.log(data)
+      this.setState({
+        data,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
   render() {
-    return (
-      <Router>
-        <div className='App'>
-          <Route path='/home' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/challenges' component={Challenges} />
+    
+    return(
+      <div className="cat">
+        <p>There are {this.state.data.length} cats in total </p>
+        <div>
+          <p>{this.state.weight}</p>
         </div>
-      </Router>
+        <div>
+         
+        </div>
+      </div>
     )
   }
 }
