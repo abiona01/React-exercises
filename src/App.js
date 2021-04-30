058a112890c87f2d9236e3bcb182906bb09f8fcc
abiona01 <quadratwemimo@gmail.com>
2021-04-30 12:57:23 +0100

    second update

--- src/App.js
+++ src/App.js
@@ -1,222 +1,32 @@
 import React, { useState } from 'react'
-import adewemimoImage from './images/Adewemimo-abiona.png'
-import './index.scss'
 
-// Fuction to show month date year
-const showDate = (time) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
+const App = () => {
+  // declaring state
+  const url =
+    'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
 
-  const month = months[time.getMonth()].slice(0, 3)
-  const year = time.getFullYear()
-  const date = time.getDate()
-  return ` ${month} ${date}, ${year}`
-}
+  const [image, setImage] = useState(url)
 
-// User Card Component
-const UserCard = ({ user: { firstName, lastName, image } }) => (
-  <div className='user-card'>
-    <img src={image} alt={firstName} />
-    <h2>
-      {firstName}
-      {lastName}
-    </h2>
-  </div>
-)
-
-// A button component
-const Button = ({ text, onClick, style }) => (
-  <button style={style} onClick={onClick}>
-    {text}
-  </button>
-)
-
-// CSS styles in JavaScript Object
-const buttonStyles = {
-  backgroundColor: '#61dbfb',
-  padding: 10,
-  border: 'none',
-  borderRadius: 5,
-  margin: 3,
-  cursor: 'pointer',
-  fontSize: 18,
-  color: 'white',
-}
-
-const Header = (props) => {
-  const {
-    welcome,
-    title,
-    subtitle,
-    author: { firstName, lastName },
-    date,
-  } = props.data
+  const changeAnimal = () => {
+    let dogURL =
+      'https://static.onecms.io/wp-content/uploads/sites/12/2015/04/dogs-pembroke-welsh-corgi-400x400.jpg'
+    let catURL =
+      'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
+    let result = image === catURL ? dogURL : catURL
+    setImage(result)
+  }
 
   return (
-    <header style={props.styles}>
-      <div className='header-wrapper'>
-        <h1>{welcome}</h1>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>
-          {firstName} {lastName}
-        </p>
-        <small>{date}</small>
+    <div className='App'>
+      <h1>30 Days Of React</h1>
+      <div className='animal'>
+        <img src={image} alt='animal' />
       </div>
-    </header>
-  )
-}
 
-const Count = ({ count, addOne, minusOne }) => (
-  <div>
-    <h1>{count} </h1>
-    <div>
-      <Button text='+1' onClick={addOne} style={buttonStyles} />
-      <Button text='-1' onClick={minusOne} style={buttonStyles} />
-    </div>
-  </div>
-)
-
-// TechList Component
-const TechList = (props) => {
-  const { techs } = props
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-  return techsFormatted
-}
-
-// Main Component
-const Main = (props) => {
-  const {
-    techs,
-    user,
-    greetPeople,
-    handleTime,
-    changeBackground,
-    count,
-    addOne,
-    minusOne,
-  } = props
-  return (
-    <main>
-      <div className='main-wrapper'>
-        <p>Prerequisite to get started react.js:</p>
-        <ul>
-          <TechList techs={techs} />
-        </ul>
-        <UserCard user={user} />
-        <Button
-          text='Greet People'
-          onClick={greetPeople}
-          style={buttonStyles}
-        />
-        <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
-        <Button
-          text='Change Background'
-          onClick={changeBackground}
-          style={buttonStyles}
-        />
-        <Count count={count} addOne={addOne} minusOne={minusOne} />
-      </div>
-    </main>
-  )
-}
-
-// Footer Component
-const Footer = (props) => {
-  return (
-    <footer>
-      <div className='footer-wrapper'>
-        <p>Copyright {props.date.getFullYear()}</p>
-      </div>
-    </footer>
-  )
-}
-
-const App = (props) => {
-  const [count, setCount] = useState(0)
-  const [backgroundColor, setBackgroundColor] = useState('')
-
-  const showDate = (time) => {
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ]
-
-    const month = months[time.getMonth()].slice(0, 3)
-    const year = time.getFullYear()
-    const date = time.getDate()
-    return ` ${month} ${date}, ${year}`
-  }
-  const addOne = () => {
-    setCount(count + 1)
-  }
-
-  // method which subtract one to the state
-  const minusOne = () => {
-    setCount(count - 1)
-  }
-  const handleTime = () => {
-    alert(showDate(new Date()))
-  }
-  const greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge, 2020')
-  }
-  const changeBackground = () => {}
-
-  const data = {
-    welcome: 'Welcome to 30 Days Of React',
-    title: 'Getting Started React',
-    subtitle: 'JavaScript Library',
-    author: {
-      firstName: 'Asabeneh',
-      lastName: 'Yetayeh',
-    },
-    date: 'Oct 7, 2020',
-  }
-  const techs = ['HTML', 'CSS', 'JavaScript']
-
-  const user = { ...data.author, image: adewemimoImage }
-
-  return (
-    <div className='app'>
-      {backgroundColor}
-      <Header data={data} />
-      <Main
-        user={user}
-        techs={techs}
-        handleTime={handleTime}
-        greetPeople={greetPeople}
-        changeBackground={changeBackground}
-        addOne={addOne}
-        minusOne={minusOne}
-        count={count}
-      />
-      <Footer date={new Date()} />
+      <button onClick={changeAnimal} class='btn btn-add'>
+        Change
+      </button>
     </div>
   )
 }
-
-
 export default App
