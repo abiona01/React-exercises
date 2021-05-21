007de7f0092bc9498ad0934728b2983cc8492887
abiona01 <quadratwemimo@gmail.com>
2021-05-21 23:59:08 +0100

    finished exercise

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,18 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Heading() {
-    return (
-        <div>
-            <div className="title">
-                <h1>30 Days of React</h1>
-                <p>Hexadecimal Colors</p>
-            </div> 
-            <div className="gene">
-                <input type="text" />
-                <button>GENERATE</button>
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<div className="title">
+				<h1 id="h1">30 Days of React</h1>
+				<p id="p">Hexadecimal Colors</p>
+			</div>
+		</div>
+	);
 }
 
-export default Heading
+export default Heading;
--- src/components/Hexacolor.js
+++ src/components/Hexacolor.js
@@ -1,59 +1,88 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
+import { FaClipboard } from "react-icons/fa";
+const hexaColor = () => {
+	let str = "0123456789abcdef";
+	let color = "";
+	for (let i = 0; i < 6; i++) {
+		let index = Math.floor(Math.random() * str.length);
+		color += str[index];
+	}
+	return "#" + color;
+};
 
-function Hexacolor() {
-	const randomHexa = () => {
-		let randomNumbers = "0123456789abcdef";
-		let color = "";
-		for (let i = 0; i < 6; i++) {
-			let index = Math.floor(Math.random() * randomNumbers.length);
-			color += randomNumbers[index];
-		}
-		return "#" + color;
+const generateColors = (n) => {
+	const colors = [];
+	for (let i = 0; i < n; i++) {
+		colors.push(hexaColor());
+	}
+	return colors;
+};
+
+const Color = ({ color, style }) => {
+	const [copied, setCopied] = useState(false);
+	const ref = useRef(null);
+	const copyResult = () => {
+		const content = ref.current.textContent;
+		navigator.clipboard.writeText(content);
+		setCopied(true);
+		setTimeout(() => {
+			setCopied(false);
+		}, 3000);
 	};
-	const generateColors = () => {
-		const colors = [];
-		for (let i = 0; i < 30; i++) {
-			colors.push(randomHexa());
+	const status = copied ? "copied" : "copy";
+	return (
+		<div style={style} className="color">
+			<small
+				ref={ref}
+				style={{ fontWeight: "500", fontSize: "32px !important" }}
+			>
+				{" "}
+				{color}
+			</small>
+			<div className={`clip-div ${status}`}>
+				<FaClipboard className="clip " onClick={copyResult} />
+			</div>
+		</div>
+	);
+};
+
+const Colors = ({ colors }) => {
+	const colorList = colors.map((color) => {
+		return (
+			<Color style={{ background: color }} color={color} className="color" />
+		);
+	});
+
+	return <div className="colors-wrapper">{colorList}</div>;
+};
+
+const ColorGeneratorApp = () => {
+	const initialColors = generateColors(27);
+	const [colors, setColors] = useState(initialColors);
+	const [value, setValue] = useState("");
+	const onChange = (e) => {
+		if (Number(e.target.value)) {
+			setValue(e.target.value);
 		}
-		return colors;
 	};
-	// const Color = ({ color, style }) => {
-	//   const [copied, setCopied] = useState(false)
-	//   const ref = useRef(null)
-	//   const copyResult = () => {
-	//     const content = ref.current.textContent
-	//     navigator.clipboard.writeText(content)
-	//     setCopied(true)
-	//     setTimeout(() => {
-	//       setCopied(false)
-	//     }, 3000)
-	//   }
-	//   const status = copied ? 'copied' : 'copy'
+	const onClick = () => {
+		const n = Number(value);
+		if (value === "" || undefined) {
+			setColors(generateColors(27));
+		} else {
+			setColors(generateColors(n));
+		}
+	};
+	return (
+		<div className="color-app">
+			<div className="gene">
+				<input type="text" onChange={onChange} value={value} />
+				<button onClick={onClick}> Generate </button>
+			</div>
 
-	//   return (
-	//     <div style={style} className='single-color'>
-	//       <small
-	//         ref={ref}
-	//         style={{ fontWeight: '500', fontSize: '32px !important' }}
-	//       >
-	//         {' '}
-	//         {color}
-	//       </small>
-	//       <div className={`wrapper__copy ${status}`}>
-	//         <i
-	//           style={{
-	//             cursor: 'pointer',
-	//             fontSize: 28,
-	//             color: 'white',
-	//           }}
-	//           class='far fa-clipboard'
-	//           onClick={copyResult}
-	//         ></i>
-	//       </div>
-	//     </div>
-	//   )
-	// }
-	return <div></div>;
-}
+			<Colors colors={colors} />
+		</div>
+	);
+};
 
-export default Hexacolor;
+export default ColorGeneratorApp;
